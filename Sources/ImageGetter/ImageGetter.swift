d226b88d16ea55632096fb8e7b4945c030eb4508
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-12-28 15:45:59 -0300

    added funcion

--- Sources/ImageGetter/ImageGetter.swift
+++ Sources/ImageGetter/ImageGetter.swift
@@ -1,6 +1,20 @@
-public struct ImageGetter {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct ImageGetter {
+    public let path: String?
+    
+    public init(path: String?) {
+        self.path = path
+    }
+    
+    @available(iOS 15.0, *)
+    public var body: some View {
+        AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w342/\(path ?? "")")) { image in
+                image.resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(maxWidth: 150, maxHeight: 250)
+        } placeholder: {
+            ProgressView()
+        }
     }
 }
